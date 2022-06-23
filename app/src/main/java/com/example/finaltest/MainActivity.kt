37513ac950a4eb25fa1078693244682602fd3735
imjaeseong <zks145@naver.com>
2022-06-23 16:12:21 +0900

    Frame Kakao

---
+++ app/src/main/java/com/example/finaltest/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.finaltest
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val viewPager2 : ViewPager2 = findViewById(R.id.viewpager2)
+        val tabLayout : TabLayout = findViewById(R.id.tabLayout)
+
+
+        viewPager2.apply {
+            adapter = MyPageradapter(context as FragmentActivity)
+        }
+
+        TabLayoutMediator(tabLayout, viewPager2) {tab, position ->
+            when (position){
+                0 -> {
+                    tab.text = "홈"
+                }
+                1 -> {
+                    tab.text = "동행 페이지"
+                }
+                2 -> {
+                    tab.text = "방명록"
+                }
+                3 -> {
+                    tab.text = "여행일지"
+                }
+                4 -> {
+                    tab.text = "마이 페이지"
+                }
+            }
+        }.attach()
+    }
+}
---
+++ app/src/main/java/com/example/finaltest/MyPageradapter.kt
@@ -0,0 +1,23 @@
+package com.example.finaltest
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class MyPageradapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+    private val NUM_PAGES = 5
+
+    override fun getItemCount(): Int = NUM_PAGES
+
+
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> MainFragment()
+            1 -> WithFragment()
+            2 -> GuestBookFragment()
+            3 -> TrapFragment()
+            else -> MyPageFragment()
+        }
+    }
+}
